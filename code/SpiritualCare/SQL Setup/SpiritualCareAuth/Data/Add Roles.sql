5c3150f710cf50fde1799a15a7c63ae1cb605607
George Nairooze <gnairooze@gmail.com>
2016-11-11 19:38:20 +0200

    update to Nov 11

---
+++ code/SpiritualCare/SQL Setup/SpiritualCareAuth/Data/Add Roles.sql
@@ -0,0 +1,26 @@
+﻿USE [SpiritualCareAuth]
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'1', N'Admin')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'2', N'AppConfig')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'3', N'AuthAdmin')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'7', N'CareContactAdmin')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'8', N'CareContactEditor')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'9', N'CareContactReader')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'4', N'PeopleAdmin')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'5', N'PeopleEditor')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'6', N'PeopleReader')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'10', N'TaskAdmin')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'11', N'TaskEditorGeneral')
+GO
+INSERT [dbo].[AspNetRoles] ([Id], [Name]) VALUES (N'12', N'TaskReaderGeneral')
+GO
